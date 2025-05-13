9a06482de200b798dcfab62616c24243ac5d3d93
Maxim Hahalin <37066384+Maksus102@users.noreply.github.com>
2025-05-13 22:04:11 +0700

    Lab12SepData

--- SepData.py
+++ SepData.py
@@ -30,8 +30,34 @@ with open("data/prepared/clean_data.csv", 'r') as file:
 train_data_70, test_data_30 = split_train_test(data, test_size=0.3)
 print(f"Разделение 70 на 30 - Тренировочная: {len(train_data_70)}, Тестовая: {len(test_data_30)}")
 
+with open("data/prepared/train_data70.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(train_data_70)
+
+with open("data/prepared/test_data30.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(test_data_30)
+
 train_data_80, test_data_20 = split_train_test(data, test_size=0.2)
 print(f"Разделение 80 на 20 - Тренировочная: {len(train_data_80)}, Тестовая: {len(test_data_20)}")
+with open("data/prepared/train_data80.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(train_data_80)
+
+with open("data/prepared/test_data20.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(test_data_20)
 
 train_data, val_data, test_data = split_train_val_test(data, test_size=0.12, k=8)
 print(f"Разделение с k=8 - Тренировочная: {len(train_data)}, Валидационная: {len(val_data)}, Тестовая: {len(test_data)}")
+with open("data/prepared/train_datak8.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(train_data)
+
+with open("data/prepared/test_datak8.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(test_data)
+
+with open("data/prepared/vak_datak8.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(val_data)
