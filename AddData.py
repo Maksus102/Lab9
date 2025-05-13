34221439695e72d0952c52b6ce908f2bafbd64cb
Maksus102 <hams102@yandex.ru>
2025-05-13 22:36:32 +0700

    lilfix

--- AddData.py
+++ AddData.py
@@ -1,15 +1,25 @@
 import csv
 
-with open("data/prepared/clean_data.csv", 'r') as file:
+with open("data/prepared/vak_datak8.csv", 'r') as file:
     reader = csv.reader(file)
     data = [row for row in reader]
 
-new_row = ["vhigh",'vhigh','3','3','small','med','unacc']
+new_row_1 = ["vhigh",'vhigh','3','3','small','med','unacc']
 
-data.append(new_row)
-print("Добавление через csv:")
-print(f"Новая строчка : {data[1725]}")
+data.append(new_row_1)
 
-with open("data/prepared/clean_data.csv", 'w', newline='') as file:
+with open("data/prepared/vak_datak8.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
+
+with open("data/prepared/train_datak8.csv", 'r') as file:
+    reader = csv.reader(file)
+    data = [row for row in reader]
+
+new_row_2 = ["vhigh",'vhigh','3','3','small','med','unacc']
+
+data.append(new_row_2)
+
+with open("data/prepared/train_datak8.csv", 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(data)
